d9a39dc98847bf32ec95d3b7b1d58bc3affdf436
Josh de Leeuw <josh.deleeuw@gmail.com>
2019-09-12 10:45:39 -0400

    Initial commit

---
+++ 1-birthday-problem.R
@@ -0,0 +1,14 @@
+# part 1 ####
+
+# suppose there are 30 students in a class. estimate the probability that at least one pair
+# students has the same birthday.
+
+# you can ignore the possibility of people being born on Feb 29 during a leap-year.
+
+
+
+
+# part 2 ####
+
+# estimate the probability for class sizes from 5-60, and plot the resulting curve 
+# (x axis is class size, y axis is probability of at least one shared birthday)
---
+++ 2-hot-potato-problem.R
@@ -0,0 +1,25 @@
+# this problem is taken from the Riddler series on fivethirtyeight.com
+# if you want some additional practice writing monte carlo simulations
+# the Riddler series questions can usually be solved with a monte carlo
+# approach.
+
+# A class of 30 children is playing a game where they all stand in a 
+# circle along with their teacher. The teacher is holding two things:
+# a coin and a potato. The game progresses like this: The teacher tosses 
+# the coin. Whoever holds the potato passes it to the left if the coin 
+# comes up heads and to the right if the coin comes up tails. The game 
+# ends when every child except one has held the potato, and the one 
+# who hasn’t is declared the winner.
+
+# How do a child’s chances of winning change depending on where they are 
+# in the circle? In other words, what is each child’s win probability? 
+
+# Visualize the final result with a graph that shows the position relative
+# to the starting location of the potato on the x-axis and the probability
+# of winning on the y-axis.
+
+# this will take some planning to think about how to formalize this problem
+# i highly recommend discussing it with a classmate! the simpler you can
+# make the abstraction of this problem, the easier it will be to write the
+# code. either way, this is a challenging problem so give yourself 
+# a high-five when you solve it.
